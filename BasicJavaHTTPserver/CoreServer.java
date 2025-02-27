26336540544e6dba214e6ca6795d40a717f6bd03
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-26 23:42:21 -0500

    server class set up

--- BasicJavaHTTPserver/CoreServer.java
+++ BasicJavaHTTPserver/CoreServer.java
@@ -3,3 +3,29 @@
 //HTTP servers use network sockets and data streams (hence the I/O devices)
 import java.net.*;
 import java.io.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Server {
+    private ServerSocket serverSocket;
+    public int port; 
+    private boolean running;
+
+    private Socket[] pool;
+    private Map<String, RequestHandler> routeMap = new HashMap<>();
+    
+    private int maxConnections = 50;
+    private int connectionTimeout = 30000; // 30 seconds
+    
+    public Server(int port) {
+        this.port = port;
+        this.connectionPool = new Socket[maxConnections]; 
+    }
+}
+
+class Main {
+    public static void main() {
+
+    }
+}
